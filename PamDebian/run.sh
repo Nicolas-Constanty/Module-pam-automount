6e8c817d2898d073115f8e3acb145fcbc59776de
Babiole <babiole@linux.home>
2016-10-05 21:27:49 +0200

    fix run.sh

--- PamDebian/run.sh
+++ PamDebian/run.sh
@@ -1,5 +1,4 @@
 echo -n "Stop " && sudo docker stop pam_test
-echo -n "Remove volume" && sudo docker rm -v pam_test
 echo -n "Remove " && sudo docker rm pam_test
 echo "Start pam_test"
 sudo docker run --privileged -i -t -v /dev:/dev -v $PWD/pam_modules/:/media/pam_modules --name pam_test pam_debian /bin/bash
