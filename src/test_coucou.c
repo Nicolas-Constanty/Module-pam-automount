4032889d3aa5487601084b6b72f2bf8246c8ec41
Babiole <babiole@linux.home>
2016-10-05 21:36:13 +0200

    little clean

--- src/test_coucou.c
+++ src/test_coucou.c
@@ -36,9 +36,8 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-const char *user;
-
-//printf("%s\n", "pam_sm_open_session");
+  //printf("%s\n", "pam_sm_open_session");
+  const char *user;
   char crypt_cmd[256] = {0};
   char fsys_cmd[256] = {0};
   char crypt_file[256] = {0};
@@ -52,7 +51,8 @@ const char *user;
   snprintf(crypt_file, 256, "/home/%s/crypt_%s", user, user);
   snprintf(vol, 256, "volume_%s", user);
   char *arguments[] = {"luksOpen", crypt_file, vol, NULL};
-  snprintf(crypt_cmd, 256, "echo -n %s | cryptsetup %s %s %s -\n", user, arguments[0], arguments[1], arguments[2]);
+  snprintf(crypt_cmd, 256, "echo -n %s | cryptsetup %s %s %s -\n",
+	   user, arguments[0], arguments[1], arguments[2]);
   system(crypt_cmd);
   snprintf(fsys_cmd, 256, "mkfs.ext4 -j /dev/mapper/volume_%s", user);
   system(fsys_cmd);
@@ -65,14 +65,14 @@ const char *user;
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
   const char *user;
-  if ( (pam_get_user(pamh, &user, NULL )) == PAM_SUCCESS )
-  {
-    printf("%s\n", user);
-  }
   char umount_cmd[256] = {0};
   char close_cmd[256] = {0};
   char clear_cmd[256] = {0};
   
+  if ( (pam_get_user(pamh, &user, NULL )) == PAM_SUCCESS )
+  {
+    printf("%s\n", user);
+  }
   snprintf(umount_cmd, 256, "umount /mnt/decrypt_%s", user);
   snprintf(close_cmd, 256, "cryptsetup luksClose volume_%s", user);
   snprintf(clear_cmd, 256, "rm -rf /mnt/decrypt_%s", user);
